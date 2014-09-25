4adcb028a8ccae7a4af5cb5c741a98531d221c8d
jgainfort <gainfortj@gmail.com>
2014-09-25 10:29:39 -0400

    challenge5 complete

--- times.js
+++ times.js
@@ -1,14 +1,51 @@
 var http = require('http');
 var async = require('async');
 
-var url = process.argv[2];
+var hostname = process.argv[2];
 var port = process.argv[3];
+var url = 'http://' + hostname + ':' + port
 
 async.series({
-    function(done){
-        var opts = {
-            hostname: url + ':' + port,
-            path: '/users/create',
-            method: 'POST'
-      };
+    post: function(done){
+        function add_user(user_id, cb){
+            var opts = {
+                hostname: hostname,
+                port: port,
+                path: '/users/create',
+                method: 'POST'
+            };
+            var req = http.request(opts, function(res){
+                res.on('data', function(chunk){});
+                res.on('end', function(){
+                    cb();
+                })
+            });
+
+            req.write(JSON.stringify({'user_id': user_id}));
+            req.end();
+        }
+
+        async.times(5, function(n, next){
+            add_user(++n, function(err){
+                next(err);
+            })
+        }, function(err){
+                if (err) return done(err);
+                done(null, 'saved');
+        });
     },
+    get: function(done){
+        var body = '';
+        http.get(url + '/users', function(res){
+            res.on('data', function(chunk){
+                body += chunk.toString();
+            });
+            res.on('end', function(){
+                done(null, body);
+            });
+        }).on('error', done);
+    }
+}, function(err, result){
+    if (err) return console.error(err);
+    console.log(result.get);
+})
